16d61aa135c1071b3e6151886a6b51fe561f0a1e
Claudia Vashchenko <78990312+claudiavashchenko@users.noreply.github.com>
2023-05-06 14:20:17 -0500

    Created js input and select values

---
+++ JS/app.js
@@ -0,0 +1,7 @@
+const primaryInput = document.querySelector("#primary-input");
+const secondaryInput = document.querySelector("#secondary-input");
+const primarySelect = document.querySelector("#primary-units");
+const secondarySelect = document.querySelector("#secondary-units");
+
+primaryInput.value = 0
+secondaryInput.value = 0
