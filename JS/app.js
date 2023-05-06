a9a776930daccb6bc4f2f73b8f629ed3f22ad796
Claudia Vashchenko <78990312+claudiavashchenko@users.noreply.github.com>
2023-05-06 14:57:59 -0500

    set up calculation

--- JS/app.js
+++ JS/app.js
@@ -6,12 +6,21 @@ const secondarySelect = document.querySelector("#secondary-units");
 primaryInput.value = 0
 secondaryInput.value = 0
 
-function update() {
-    
+function update(e) {
+    const elementId = e.target.id
+    if(elementId === "primary-input" || elementId === "primary-units" || elementId === "secondary-units") {
+       secondaryInput.value = 10
+    }
+    if(elementId === "secondary-input") {
+        primaryInput.value = calculate()
+    } else {
+        secondaryInput.value = calculate(primarySelect.value, secondarySelect.value, primaryInput.value)
+    }
 }
 
-function calculate() {
+function calculate(firstTempUnit, secondTempUnit, temp) {
 
+    return 100
 }
 
 primaryInput.addEventListener("change", update)
